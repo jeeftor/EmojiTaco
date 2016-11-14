01d584d4878dc004f032027ffbedd0a928988515
Jeff Stein <jstein@mitre.org>
2016-11-13 20:27:47 -0500

    Fixed search results counter

--- esearch.py
+++ esearch.py
@@ -47,7 +47,7 @@ def main(wf):
     if len(name_match + keyword_match) == 0:
         wf.add_item("\\U0001F622\\U0000FE0F".decode('unicode_escape') + ' No Emoji found', 'Please try again', icon='icon.png')
     else:
-        if len > 9:
+        if len(name_match + keyword_match) > 9:
             wf.add_item( str(len(name_match + keyword_match)) + ' matches found', 'To remove results use - in front of a term',
                          icon='icon.png', valid=False)
     for array in name_match + keyword_match:
